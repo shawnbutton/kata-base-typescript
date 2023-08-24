3fcb4983f8ef6b9abf055c7f13eead6c3d7de571
shawn <shawnbutton@gmail.com>
2023-08-24 11:19:51 -0400

    added files for typescript version of empty kata

---
+++ src/kata.ts
@@ -0,0 +1,5 @@
+export class Kata {
+    someFunction() {
+        return "not implemented";
+    }
+}
---
+++ test/kata.spec.ts
@@ -0,0 +1,10 @@
+import {Kata} from '../src/kata'
+
+describe('Empty Kata', () => {
+
+    it('should pass this test', () => {
+        const sut = new Kata();
+        expect(sut.someFunction()).toEqual('expectation');
+    })
+
+});
